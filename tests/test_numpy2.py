63449bf052415bbfcda23e35371ab8281feee7ea
BoshraJaber <boshrajaber94@gmail.com>
2021-10-21 11:15:40 +0300

    building the tests

--- tests/test_numpy2.py
+++ tests/test_numpy2.py
@@ -1,5 +1,13 @@
 from numpy2 import __version__
+from numpy2 import chessBoard
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_same_row():
+    new_chess=chessBoard()
+    new_chess.add_red(2,6)
+    new_chess.add_blue(2,2)
+    assert new_chess.is_under_attack() == True
+
